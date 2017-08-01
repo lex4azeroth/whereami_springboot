3ae19692d8a81f1dd5c26813a00d9eb9d85c49b1
lex <mxhray@hotmail.com>
2017-08-01 22:19:06 +0800

    add map layer

--- src/main/resources/static/js/zyzb.js
+++ src/main/resources/static/js/zyzb.js
@@ -81,7 +81,7 @@ function createShowingTable(data) {
 	var len = data.length;
 	for (var i = 0; i < len; i++) {
 		tableStr = tableStr 
-		+ "<tr>" 
+		+ "<tr onclick='showMapLayer(this)'>" 
 			+ "<td>" + data[i].guid + "</td>"
 			//                +"<td><input class='check' id='checkOne' name='checkOne' type='checkbox' value='"+data[i].key+"' /></td>"  
 			+ "<td>" + data[i].cfcc + "</td>" 
@@ -95,7 +95,7 @@ function createShowingTable(data) {
 	if (len == 0) {
 		tableStr = tableStr 
 		+ "<tr style='text-align: center'>"
-			+ "<td colspan='6'><font color='#cd0a0a'>" + 暂无记录 + "</font></td>" 
+		+ "<td colspan='6'><font color='#cd0a0a'>暂无记录</font></td>" 
 		+ "</tr>";
 	}
 	tableStr = tableStr + "</table>";
@@ -106,10 +106,76 @@ function cleanValidators() {
 	$('#fromValidator').text("");
 	$('#endValidator').text("");
 }
+
+function showMapLayer(obj) {
+	var guid = obj.childNodes[0].innerText;
+	var zykssj = obj.childNodes[3].innerText;
+	var zyjssj = obj.childNodes[4].innerText;
+	var gd = obj.childNodes[6].innerText;
+	$("#myMap").addClass("float_map");
+	var container = "<div id='container' class='float_map'></div><div><a href='javascript:;' class='float_close' data-role='button' onclick='closeMapLayer()'>关闭</a></div>";
+	$("#mapContainer").html(container);
+	$("#myMap").css("display", "block");
+//	showRealMap();
+	var restURL = "ihavebeenoriginal/" + guid + "/" + zykssj + "/" + zyjssj + "/" + gd;
+	whereami(restURL);
+}
+
+function closeMapLayer() {
+	$("#myMap").css("display", "none");
+}
+
+function showRealMap() {
+	var map = new BMap.Map("container",{mapType: BMAP_SATELLITE_MAP});      //设置卫星图为底图
+	var point = new BMap.Point(121.323258,31.284051);    // 创建点坐标
+	map.centerAndZoom(point, 19);                     // 初始化地图,设置中心点坐标和地图级别。
+	map.addControl(new BMap.NavigationControl());
+	}
+
+function whereami(restURL) {
+//	var restURL = "ihavebeen/" + equipmentId + "/" + fromDateTime + "/" + endDateTime + "/" + lineNum;
+	var lols = new Array();
+	    $.ajax({
+			type: "GET",
+	        url: restURL,
+			async: true,
+			contentType: "application/json",
+	        cache:false,
+			success: function(data) {
+	            lols = data;
+	            showRouteWithLol(data);
+			}
+		});
+}
 	
+function showRouteWithLol(lols) {
+	var clon = parseFloat(lols[0].longitude);
+	var clat = parseFloat(lols[0].latitude);
+
+	var map = new BMap.Map("container",{mapType: BMAP_SATELLITE_MAP});      //设置卫星图为底图
+	var point = new BMap.Point(clon, clat);    // 创建点坐标
+	map.centerAndZoom(point, 19);                     // 初始化地图,设置中心点坐标和地图级别。
+	map.addControl(new BMap.NavigationControl());
+	map.enableScrollWheelZoom();                  // 启用滚轮放大缩小。
+	// map.enableKeyboard();                         // 启用键盘操作。  
+	// map.setCurrentCity("北京");          // 设置地图显示的城市 此项是必须设置的
+	var marker = new BMap.Marker(point);
+	// map.addOverlay(marker);               // 将标注添加到地图中
+	marker.setAnimation(BMAP_ANIMATION_BOUNCE); //跳动的动画
+	map.addOverlay(marker); 
+
+	var polyLine = new Array();
+
+	for (var i = 0; i < lols.length - 1; i++) {
+	    var lon = parseFloat(lols[i].longitude);
+	    var lat = parseFloat(lols[i].latitude);
+	    polyLine.push(new BMap.Point(lon, lat));
+	}
+
+	var polyline = new BMap.Polyline(polyLine, {strokeColor:"yellow", strokeWeight:1, strokeOpacity:0.9});
+	map.addOverlay(polyline);
+	}
 $(document).ready(function() {
-		showDeviceList();
-		
 		$('#fromDate').val(moment().format('YYYY-MM-DD'));
 		$('#endDate').val(moment().format('YYYY-MM-DD'));
 		$('#fromTime').val(moment().format('hh:mm:ss'));
